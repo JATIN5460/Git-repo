569c4a61d11a3408ef5cbee7229b73d0418f5e16
Jatin Jasrotia <119511849+JATIN5460@users.noreply.github.com>
2024-08-09 22:04:45 +0530

    code.cpp

---
+++ code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int func(int a,int b){
+    return a+b;
+}
+int main(){
+
+cout<<"the sum of the two numbers would be:"<<func(34,43)<<endl;
+    
+    return 0;
+}
